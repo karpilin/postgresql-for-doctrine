b3d5471ea66cf5def025d18b3d4448e12b4287c9
Andrei Karpilin <karpilin@gmail.com>
2025-05-23 19:14:24 +0100

    test

--- tests/Unit/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ToCharTest.php
+++ tests/Unit/MartinGeorgiev/Doctrine/ORM/Query/AST/Functions/ToCharTest.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Unit\MartinGeorgiev\Doctrine\ORM\Query\AST\Functions;
 
-use Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsTexts;
+use Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsDates;
+use Fixtures\MartinGeorgiev\Doctrine\Entity\ContainsDecimals;
 use MartinGeorgiev\Doctrine\ORM\Query\AST\Functions\ToChar;
 
 class ToCharTest extends TestCase
@@ -19,14 +20,16 @@ class ToCharTest extends TestCase
     protected function getExpectedSqlStatements(): array
     {
         return [
-            "SELECT to_char(c0_.text1, '999D99S') AS sclr_0 FROM ContainsTexts c0_",
+            "SELECT to_char(c0_.datetime1, 'HH12:MI:SS') AS sclr_0 FROM ContainsDates c0_",
+            "SELECT to_char(c0_.decimal1, '999D99S') AS sclr_0 FROM ContainsDecimals c0_",
         ];
     }
 
     protected function getDqlStatements(): array
     {
         return [
-            \sprintf("SELECT TO_CHAR(e.text1, '999D99S') FROM %s e", ContainsTexts::class),
+            \sprintf("SELECT TO_CHAR(e.datetime1, 'HH12:MI:SS') FROM %s e", ContainsDates::class),
+            \sprintf("SELECT TO_CHAR(e.decimal1, '999D99S') FROM %s e", ContainsDecimals::class),
         ];
     }
 }
